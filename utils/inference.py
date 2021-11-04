6d0a29053717760b6119d439436fe070c74d3fb1
Jimmy_Tempest <smartwattana@gmail.com>
2021-11-05 00:07:45 +0700

    finish jupyter based on https://www.youtube.com/watch?v=CSpsB4_-5WQ

---
+++ utils/inference.py
@@ -0,0 +1,24 @@
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input
+
+
+enc_model = Model([enc_inp], enc_states)
+
+
+
+# decoder Model
+decoder_state_input_h = Input(shape=(400,))
+decoder_state_input_c = Input(shape=(400,))
+
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+
+
+decoder_outputs, state_h, state_c = dec_lstm(dec_embed , 
+                                    initial_state=decoder_states_inputs)
+
+
+decoder_states = [state_h, state_c]
+
+
+dec_model = Model([dec_inp]+ decoder_states_inputs,
+                                      [decoder_outputs]+ decoder_states)
---
+++ utils/preprocess.py
@@ -0,0 +1,17 @@
+import re
+def clean_text(txt):
+    txt = txt.lower()
+    txt = re.sub(r"i'm", "i am", txt)
+    txt = re.sub(r"he's", "he is", txt)
+    txt = re.sub(r"she's", "she is", txt)
+    txt = re.sub(r"that's", "that is", txt)
+    txt = re.sub(r"what's", "what is", txt)
+    txt = re.sub(r"where's", "where is", txt)
+    txt = re.sub(r"\'ll", " will", txt)
+    txt = re.sub(r"\'ve", " have", txt)
+    txt = re.sub(r"\'re", " are", txt)
+    txt = re.sub(r"\'d", " would", txt)
+    txt = re.sub(r"won't", "will not", txt)
+    txt = re.sub(r"can't", "can not", txt)
+    txt = re.sub(r"[^\w\s]", "", txt)
+    return txt
